2761c120e91201af72e5a76bb63a8207e1f0505a
mycwnet <36975600+mycwnet@users.noreply.github.com>
2018-09-18 23:18:38 -0700

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,11 +3,8 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 ######################################
-# Basic PHP scraper
-# Credit to Yukoff this code is forked from their scrapper
-# https://github.com/yukoff/imdb-toptv-250
-# I couldn't use most of that data an wanted the ID (which that scraper lacked)
-# and Rank Only
+#  Scrapes top 250 movies from imdb
+#  Outputs rank and id
 ######################################
 $html = scraperwiki::scrape("http://www.imdb.com/chart/top");
 
@@ -19,18 +16,8 @@ $ids=[];
 foreach($dom->find('td.titleColumn') as $movie) {
 
    preg_match('#.*?([1-9][0-9]{0,2})\..*?<a.*?\/title\/(.+?)\/.*#', $movie->innertext, $match);
-    $ids[$match[1]]=$match[2];
+    $ids[]=['rank'=>match[1],'imdb_id'=>$match[2]];
 }
-print_r($ids);
+scraperwiki::save_sqlite(['rank'],$ids)
 
-function clean($val)
-{
-    $val = str_replace('&nbsp;', ' ', $val);
-    $val = str_replace('&amp;', '&', $val);
-    $val = html_entity_decode($val);
-    $val = strip_tags($val);
-    $val = trim($val);
-    $val = utf8_decode($val);
-    return $val;
-}
 ?>
