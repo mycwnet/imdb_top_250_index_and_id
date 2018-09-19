1cafc652c52b07ebb813bf7f06d8244027f424b2
mycwnet <36975600+mycwnet@users.noreply.github.com>
2018-09-18 22:36:52 -0700

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -15,18 +15,14 @@ $dom = new simple_html_dom();
 $dom->load($html);
 
 $movies=$dom->find('.titleColumn');
-preg_match_all('|<tr bgcolor="#.*?" valign="top"><td align="right"><font face="Arial, Helvetica, sans-serif" size="-1"><b>(.*?)\.</b></font></td><td align="center"><font face="Arial, Helvetica, sans-serif" size="-1">.*?</font></td><td><font face="Arial, Helvetica, sans-serif" size="-1"><a href="(.*?)">.*?</a> \(.*?\)</font></td><td align="right"><font face="Arial, Helvetica, sans-serif" size="-1">.*?</font></td></tr>|', $html, $arr);
-foreach ($arr[1] as $key => $val) {
-    scraperwiki::save_sqlite([
-        'rank'
-    ], [
-        'rank' => "" . clean($arr[1][$key]),
-        'imdb_id' => clean($arr[2][$key])
-    ]);
-    echo "rank: " . clean($arr[1][$key]);
-    echo "id: " .clean($arr[2][$key]);
+$ids=[];
+foreach($movies->find('a') as $link) {
+    echo $movies->plaintext;
+   preg_match('/.*?\/title\/(.+)?\/', $link->href, $match);
+    $ids[]=$match[1];
 }
- print_r($movies);
+print_r($ids);
+
 function clean($val)
 {
     $val = str_replace('&nbsp;', ' ', $val);
@@ -36,11 +32,5 @@ function clean($val)
     $val = trim($val);
     $val = utf8_decode($val);
     return $val;
-}
-function oneline($code)
-{
-    $code = str_replace("\n", '', $code);
-    $code = str_replace("\r", '', $code);
-    return $code;
 }
 ?>
