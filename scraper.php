2d7558ebebeba15894c58daea59b8e0d401b2846
mycwnet <36975600+mycwnet@users.noreply.github.com>
2018-09-25 19:38:21 -0700

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,6 +5,8 @@ require 'scraperwiki/simple_html_dom.php';
 ######################################
 #  Scrapes top 250 movies from imdb
 #  Outputs rank and id
+#  Scraper wiki seems broken, the save function doesn't
+#  seem to honor unique keys.
 ######################################
 $html = scraperwiki::scrape("http://www.imdb.com/chart/top");
 
@@ -12,7 +14,6 @@ $dom = new simple_html_dom();
 $dom->load($html);
 
 $movies=$dom->find('.titleColumn');
-$movies_list=[];
 foreach($dom->find('td.titleColumn') as $movie) {
 
    preg_match('#.*?([1-9][0-9]{0,2})\..*?<a.*?\/title\/(.+?)\/.*#', $movie->innertext, $match);
