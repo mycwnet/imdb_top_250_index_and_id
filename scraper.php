76276b9f184412928f30d5ecc2409fdc81fb90d1
mycwnet <36975600+mycwnet@users.noreply.github.com>
2018-09-25 19:09:51 -0700

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -10,15 +10,13 @@ $html = scraperwiki::scrape("http://www.imdb.com/chart/top");
 
 $dom = new simple_html_dom();
 $dom->load($html);
-// clear the DB, for some reason scraperwiki fails to honor unique keys?
-scraperwiki::sqliteexecute("DELETE from data");
-scraperwiki::sqlitecommit();
+
 $movies=$dom->find('.titleColumn');
+$movies_list=[];
 foreach($dom->find('td.titleColumn') as $movie) {
 
    preg_match('#.*?([1-9][0-9]{0,2})\..*?<a.*?\/title\/(.+?)\/.*#', $movie->innertext, $match);
-  
-   scraperwiki::save_sqlite(['rank','imdb_id'],['rank'=>$match[1],'imdb_id'=>$match[2]]);
+  $movies_list[]=['rank'=>$match[1],'imdb_id'=>$match[2]];
 }
-
+ scraperwiki::save_sqlite(['rank','imdb_id'],$movies_list);
 ?>
